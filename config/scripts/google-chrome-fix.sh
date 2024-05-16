7465cb3e3062c0acca21a0db39aaa264f1604a8e
Shantanu Shukla <shantanu.shukla10@gmail.com>
2024-05-16 19:01:01 +0530

    fix build

--- config/scripts/google-chrome-fix.sh
+++ config/scripts/google-chrome-fix.sh
@@ -15,15 +15,5 @@ echo "Disable gpgcheck for google-chrome repo"
 # Disable gpgcheck
 sed -i '/gpgcheck/d' /etc/yum.repos.d/google-chrome.repo
 echo "gpgcheck=0" >> /etc/yum.repos.d/google-chrome.repo
+sed -i '/skip_if_unavailable/d' /etc/yum.repos.d/google-chrome.repo
 echo $(cat /etc/yum.repos.d/google-chrome.repo)
-
-# First, delete all old keys; see https://github.com/rpm-software-management/rpm/issues/2577
-GOOGLE_PUBKEYS_RPMS=$(rpm -qa gpg-pubkey* --qf '%{NAME}-%{VERSION}-%{RELEASE} %{PACKAGER}\n' | grep 'linux-packages-keymaster@google.com' | sed 's/ .*$//' | xargs rpm -e)
-if [ -n $GOOGLE_PUBKEYS_RPMS ]; then
-    echo "Removing pakcages $GOOGLE_PUBKEYS_RPMS"
-    rpm -e $GOOGLE_PUBKEYS_RPMS
-fi
-echo "Downloading Google Signing Key"
-curl https://dl.google.com/linux/linux_signing_key.pub > /tmp/linux_signing_key.pub
-
-rpm --import /tmp/linux_signing_key.pub
